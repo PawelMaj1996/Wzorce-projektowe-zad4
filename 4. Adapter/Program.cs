d4305d99fad72c96c84632c82fdb332e48328038
PawelMaj1996 <149311165+PawelMaj1996@users.noreply.github.com>
2024-06-09 08:23:17 +0200

    Add files via upload

---
+++ 4. Adapter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+interface IShape
+{
+    double GetArea();
+}
+
+class Square
+{
+    private double side;
+
+    public Square(double side)
+    {
+        this.side = side;
+    }
+
+    public double GetSide()
+    {
+        return side;
+    }
+
+    public void SetSide(double side)
+    {
+        this.side = side;
+    }
+
+    public double CalculateArea()
+    {
+        return side * side;
+    }
+}
+
+class SquareAdapter : IShape
+{
+    private Square square;
+
+    public SquareAdapter(Square square)
+    {
+        this.square = square;
+    }
+
+    public double GetArea()
+    {
+        return square.CalculateArea();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Square square = new Square(5.0);
+        IShape shape = new SquareAdapter(square);
+
+        Console.WriteLine("Area of the square: " + shape.GetArea());
+    }
+}
